33fc465a32b81fd5a350508daec6ecbdb9dbc384
PrarthitShah <prarthit.shah@graas.ai>
2024-04-23 23:51:47 +0530

    added changes in backedn

--- task-management-backend/app.py
+++ task-management-backend/app.py
@@ -29,9 +29,13 @@ class TasksListCreate(Resource):
     def get(self):
         tasks = database.Tasks.find({})
         if not tasks:
-            return {"message": "Tasks data is not found"}, 404
+            return {"message": "Tasks data is not found", "statusCode": 404}, 404
 
-        return {"message": "Tasks fetched successfully", "data": list(tasks)}, 200
+        return {
+            "message": "Tasks fetched successfully",
+            "data": list(tasks),
+            "statusCode": 200,
+        }, 200
 
     ############################## Create Task #############################
     def post(self):
@@ -39,13 +43,19 @@ class TasksListCreate(Resource):
 
         content = dict(request.json)
         if not content:
-            return {"message": "Bad request. Please enter valid data"}, 400
+            return {
+                "message": "Bad request. Please enter valid data",
+                "statusCode": 400,
+            }, 400
 
         content.update({"_id": _id})
 
         # Added checks for title and status
         if not content.get("title", ""):
-            return {"message": "Bad request. Please enter title for task"}, 400
+            return {
+                "message": "Bad request. Please enter title for task",
+                "statusCode": 400,
+            }, 400
 
         if content.get("status", "") not in (
             "To Do",
@@ -53,16 +63,20 @@ class TasksListCreate(Resource):
             "Done",
         ):
             return {
-                "message": "Bad request. Please enter valid status for task - ('To Do','In Progress','Done')"
+                "message": "Bad request. Please enter valid status for task - ('To Do','In Progress','Done')",
+                "statusCode": 400,
             }, 400
 
         result = database.Tasks.insert_one(content)
         if not result.inserted_id:
-            return {"message": "Failed to insert"}, 500
+            return {"message": "Failed to insert", "statusCode": 500}, 500
 
-        # task = database.Tasks.find({"_id": result.inserted_id})
         tasks = database.Tasks.find({})
-        return {"message": "Task Inserted Successfully", "data": list(tasks)}, 200
+        return {
+            "message": "Task Inserted Successfully",
+            "data": list(tasks),
+            "statusCode": 200,
+        }, 200
 
 
 class TasksRetrieveUpdateDelete(Resource):
@@ -72,9 +86,13 @@ class TasksRetrieveUpdateDelete(Resource):
         task = database.Tasks.find_one(query)
 
         if not task:
-            return {"message": "Task data is not found"}, 404
+            return {"message": "Task data is not found", "statusCode": 404}, 404
 
-        return {"message": "Successfully fetched data", "data": task}, 200
+        return {
+            "message": "Successfully fetched data",
+            "data": task,
+            "statusCode": 200,
+        }, 200
 
     ############################## Update Single Task #############################
     def put(self, _id):
@@ -83,7 +101,10 @@ class TasksRetrieveUpdateDelete(Resource):
         if content and query:
             # Added checks for title and status
             if not content.get("$set", dict()).get("title", ""):
-                return {"message": "Bad request. Please enter title for task"}, 400
+                return {
+                    "message": "Bad request. Please enter title for task",
+                    "statusCode": 400,
+                }, 400
 
             if content.get("$set", dict()).get("status", "") not in (
                 "To Do",
@@ -91,23 +112,32 @@ class TasksRetrieveUpdateDelete(Resource):
                 "Done",
             ):
                 return {
-                    "message": "Bad request. Please enter valid status for task - ('To Do','In Progress','Done')"
+                    "message": "Bad request. Please enter valid status for task - ('To Do','In Progress','Done')",
+                    "statusCode": 400,
                 }, 400
 
             result = database.Tasks.update_one(query, content)
 
             if not result.matched_count:
-                return {"message": "Failed to update. Record is not found"}, 409
+                return {
+                    "message": "Failed to update. Record is not found",
+                    "statusCode": 409,
+                }, 409
 
             if not result.modified_count:
-                return {"message": "No changes applied"}, 500
+                return {"message": "No changes applied", "statusCode": 500}, 500
         else:
             return {
-                "message": "No changes applied! Current and Requested both the objects are the same"
+                "message": "No changes applied! Current and Requested both the objects are the same",
+                "statusCode": 405,
             }, 405
 
         tasks = database.Tasks.find({})
-        return {"message": "Task Updated successfully", "data": list(tasks)}, 200
+        return {
+            "message": "Task Updated successfully",
+            "data": list(tasks),
+            "statusCode": 200,
+        }, 200
 
     ############################## Delete Single Task #############################
     def delete(self, _id):
@@ -116,12 +146,16 @@ class TasksRetrieveUpdateDelete(Resource):
         tasks = database.Tasks.find({})
 
         if not result.deleted_count:
-            return {"message": "Task data is not found!"}, 500
+            return {"message": "Task data is not found!", "statusCode": 500}, 500
 
         if not tasks:
-            return {"message": "Tasks data is not found"}, 404
+            return {"message": "Tasks data is not found", "statusCode": 404}, 404
 
-        return {"message": "Task Deleted successfully", "data": list(tasks)}, 200
+        return {
+            "message": "Task Deleted successfully",
+            "data": list(tasks),
+            "statusCode": 200,
+        }, 200
 
 
 class Register(Resource):
@@ -131,24 +165,42 @@ class Register(Resource):
         password = request.json.get("password", "")
 
         if not email:
-            return {"message": "Bad request. Please Enter email address"}, 400
+            return {
+                "message": "Bad request. Please Enter email address",
+                "statusCode": 400,
+            }, 400
 
         if not checkEmailValidation(email):
-            return {"message": "Bad request. Please Enter valid email"}, 400
+            return {
+                "message": "Bad request. Please Enter valid email",
+                "statusCode": 400,
+            }, 400
 
         if not password:
-            return {"message": "Bad request. Please Enter password"}, 400
+            return {
+                "message": "Bad request. Please Enter password",
+                "statusCode": 400,
+            }, 400
 
         if not len(password) >= 8:
-            return {"message": "Bad request. Enter password with 8 character"}, 400
+            return {
+                "message": "Bad request. Enter password with 8 character",
+                "statusCode": 400,
+            }, 400
 
         # Check if the username already exists
         if database.Users.find_one({"username": email}):
-            return {"message": "Username already exists. Choose a different one."}, 400
+            return {
+                "message": "Username already exists. Choose a different one.",
+                "statusCode": 400,
+            }, 400
         else:
             database.Users.insert_one({"username": email, "password": password})
 
-        return {"message": "Registration successful. You can now log in."}, 200
+        return {
+            "message": "Registration successful. You can now log in.",
+            "statusCode": 200,
+        }, 200
 
 
 class Login(Resource):
@@ -158,23 +210,38 @@ class Login(Resource):
         password = request.json.get("password", "")
 
         if not email:
-            return {"message": "Bad request. Please Enter email address"}, 400
+            return {
+                "message": "Bad request. Please Enter email address",
+                "statusCode": 400,
+            }, 400
 
         if not checkEmailValidation(email):
-            return {"message": "Bad request. Please Enter valid email"}, 400
+            return {
+                "message": "Bad request. Please Enter valid email",
+                "statusCode": 400,
+            }, 400
 
         if not password:
-            return {"message": "Bad request. Please Enter password"}, 400
+            return {
+                "message": "Bad request. Please Enter password",
+                "statusCode": 400,
+            }, 400
 
         if not len(password) >= 8:
-            return {"message": "Bad request. Enter password with 8 character"}, 400
+            return {
+                "message": "Bad request. Enter password with 8 character",
+                "statusCode": 400,
+            }, 400
 
         # Check if the username and password match
         user = database.Users.find_one({"username": email, "password": password})
         if not user:
-            return {"message": "Invalid username or password. Please try again."}
+            return {
+                "message": "Invalid username or password. Please try again.",
+                "statusCode": 500,
+            }, 500
 
-        return {"message": "Logged in Successfully"}, 200
+        return {"message": "Logged in Successfully", "statusCode": 200}, 200
 
 
 # Initialize Endpoints
